c180ce9f34d185ae017d238cd051cc16c4322201
Abhay Kumar Singh <148642793+Abhay52004@users.noreply.github.com>
2025-10-02 12:14:49 +0530

    Automated upload: pattern21.c

---
+++ pattern21.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    for(int i = 1;i<=4 ;i++){
+        for(int j = 1 ;j<=4-i ; j++){
+            printf(" ");
+        }
+        for(int k = i ; k>=1 ;k--)
+        printf("%d",k);
+        for(int l = 2 ; l<=i ;l++)
+        printf("%d",l);
+        printf("\n");
+    }
+
+    system("pause");
+    return 0;
+}
